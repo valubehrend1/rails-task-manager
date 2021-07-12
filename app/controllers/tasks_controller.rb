fe3b65debb62999c012c778785ccb2a10099f230
Valeria Behrend <valu-sp@hotmail.com>
2021-07-12 23:02:31 +0200

    all actions working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,12 +17,19 @@ class TasksController < ApplicationController
     end
 
     def edit
+        @task = Task.find(params[:id])
     end
 
     def update
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+        redirect_to task_path(@task)
     end
 
-    def delete
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to tasks_path
     end
 
     private
