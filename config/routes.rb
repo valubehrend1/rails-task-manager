6b6247faa7c5114b2146d056880d4df0680fc14f
Valeria Behrend <valu-sp@hotmail.com>
2021-07-13 01:26:51 +0200

    style

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #read
-  get 'tasks', to: 'tasks#index', as: :tasks
+  get '/', to: 'tasks#index', as: :tasks
 
   #create
   post 'tasks', to: 'tasks#create', as: :tasks_create
